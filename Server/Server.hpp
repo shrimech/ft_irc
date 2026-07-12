75a10c9a1a09476242253fe337a1cb14340572e1
Achraf Hamou tahar <ahamou-t@c2r4p2.1337.ma>
2026-07-12 06:51:19 +0100

    class server created and project is organized

---
+++ Server/Server.hpp
@@ -0,0 +1,38 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <fcntl.h>
+#include <unistd.h>
+#include <poll.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
+#include <cerrno>
+
+class Server {
+private:
+    int _port;
+    int _serverSocket;
+    std::string _serverName;
+    std::string _password;
+    std::vector<struct pollfd> _pollfds;
+    std::map<int, std::string> _clientBuffers; // atwali std::map<int, Client> (a si salah)
+
+    void AcceptNewClient();
+    void ReceiveNewData(int fd);
+    void ClearClient(int fd);
+
+public:
+    Server(int port, std::string password);
+    std::string getName() const;
+    ~Server();
+
+    void Init(); // Set up socket, bind, listen
+    void Run();  // poll() loop
+};
+
+#endif
---
+++ Server/main.cpp
@@ -0,0 +1,21 @@
+#include "Server.hpp"
+#include <cstdlib>
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: ./server <port> <password>" << std::endl;
+        return 1;
+    }
+
+    try {
+        Server irc_server(std::atoi(argv[1]), argv[2]);
+        irc_server.Init();
+        irc_server.Run();
+    } 
+    catch (const std::exception& e) {
+        std::cerr << "Fatal Error: " << e.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
